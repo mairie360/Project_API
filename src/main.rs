b420b07d5452b1cbbf246fa77399911c8f01e96a
Quentin Tennerel <tennerelquentin@gmail.com>
2026-03-11 18:28:19 +0800

    feat: add openapi gen

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use actix_web::{get, middleware, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{middleware, web, App, HttpServer};
 
 use project_api::auth_middleware::JwtMiddleware;
+use project_api::swagger::{health, hello, ApiDoc};
 
 use mairie360_api_lib::database::db_interface::{get_db_interface, init_db_interface};
 use mairie360_api_lib::env_manager::get_critical_env_var;
@@ -8,54 +9,6 @@ use mairie360_api_lib::redis::redis_manager::{create_redis_manager, get_redis_ma
 
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
-
-//                                        -- POST REQUESTS --
-
-/** * Handles a POST request to the root endpoint.
- * Responds with a simple "Hello, world!" message.
- */
-#[utoipa::path(
-    post,
-    path = "/",
-    responses(
-        (status = 200, description = "Returns a greeting message", body = String)
-    )
-)]
-#[post("/")]
-pub async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello, world!")
-}
-
-//                                        -- GET REQUESTS --
-
-/** * Handles a GET request to the /health endpoint.
- * Responds with a simple "OK" message to indicate the service is healthy.
- */
-#[utoipa::path(
-    get,
-    path = "/health",
-    responses(
-        (status = 200, description = "Service is healthy", body = String)
-    )
-)]
-#[get("/health")]
-pub async fn health() -> impl Responder {
-    HttpResponse::Ok().body("OK")
-}
-
-#[derive(OpenApi)]
-#[openapi(
-    paths(
-        health,
-        hello
-    ),
-    components(
-    ),
-    tags(
-        (name = "Template API", description = "Endpoints for templatefunctionalities")
-    )
-)]
-struct ApiDoc;
 
 //                                        -- MAIN FUNCTION --
 
@@ -104,7 +57,7 @@ async fn main() -> std::io::Result<()> {
             // get requests
             .service(health)
             // Middleware
-            // .service(web::scope("").wrap(JwtMiddleware).service(example))
+            .service(web::scope("").wrap(JwtMiddleware))
             // API documentation
             .service(
                 SwaggerUi::new("/swagger-ui/{_:.*}")
